acec79cd03fef509a1c4fd27e1ba7efbbfb4ec3c
Giancarlo Zuliani <slaigox@gmail.com>
2021-02-02 17:34:00 +0100

    Routes and controller added

--- main/app/Http/Controllers/DrinkController.php
+++ main/app/Http/Controllers/DrinkController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Drink;
 
 class DrinkController extends Controller
 {
-    //
+    public function index(){
+        $drinks = Drink::all();
+        return view('pages.index' , compact('drinks'));
+    }
+    public function show($id){
+        $drink = Drink::findOrFail($id);
+        return view('pages.show-drink', compact('drink'));
+    }
+    public function create(){
+        return view('pages.create-drink');
+    }
+    public function store(Request $request){
+        Drink::create($request -> all());
+        return redirect() -> route('home-drink');
+    }
+    public function destroy($id){
+        Drink::destroy($id);
+        return redirect() -> route('home-drink');
+    }
+    public function edit($id){
+        $drink = Drink::findOrFail($id);
+        return view('pages.edit-drink' , compact('drink'));
+    }
+    public function update(Request $request,$id){
+        $drink = Drink::findOrFail($id);
+        $drink -> update($request -> all());
+        return redirect() -> route('home-drink');
+    }
 }
--- main/routes/web.php
+++ main/routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/' , 'DrinkController@index')
+    -> name('home-drink');
+Route::get('/show/{id}' , 'DrinkController@show')
+    -> name('show-drink');
+Route::get('/create' , 'DrinkController@create')
+    -> name('create-drink');
+Route::post('/store' , 'DrinkController@store')
+    -> name('store-drink');
+Route::get('/edit/{id}' , 'DrinkController@edit')
+    -> name('edit-drink');
+Route::post('/update/{id}' , 'DrinkController@update')
+    -> name('update-drink');
